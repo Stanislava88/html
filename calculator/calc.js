31d22e7239138ddae96c587cd425339661c04405
Krasimir Raikov <raikov.krasimir@gmail.com>
2016-05-11 09:53:12 +0300

    resolved NaN and Infinity problems, plus 021=17

--- calculator/calc.js
+++ calculator/calc.js
@@ -29,13 +29,25 @@ function calculate() {
     var fieldValue = field.value;
     var fieldLength = fieldValue.length - 1;
     var lastChar = fieldValue.charAt(fieldLength);
+    var firstCharZero = fieldValue.charAt(0) == '0';
+    if (firstCharZero && fieldValue.charAt(1) != "/" && fieldValue.charAt(1) != "*" && fieldValue.charAt(1) != "+" && fieldValue.charAt(1) != "-") {
+        while (fieldValue.charAt(0) == '0') {
+            field.value = fieldValue.slice(1, fieldValue.length + 1);
+            fieldValue = field.value;
+        }
+    }
     if (lastChar == "+" || lastChar == "-" || lastChar == "/" || lastChar == "*") {
         field.value = fieldValue.slice(0, fieldValue.length - 1);
     }
     if (fieldLength == -1) {
         field.value = 0;
     } else {
-        field.value = eval(field.value);
+        var fieldEval = eval(field.value);
+        if (isFinite(fieldEval)) {
+            field.value = isNaN(fieldEval) ? 0 : fieldEval;
+        } else {
+            field.value = 0;
+        }
     }
 }
 
