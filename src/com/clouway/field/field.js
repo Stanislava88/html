cef27b1f13e27c6f16c13c32187eedd0112e88fe
Stanislava Kaukova <sisiivanovva@gmail.com>
2016-05-31 09:58:32 +0300

    Renamed

--- src/com/clouway/field/field.js
+++ src/com/clouway/field/field.js
@@ -1,14 +1,11 @@
 /**
  * Created by clouway on 30.05.16.
  */
-function validateField() {
-    var x = document.getElementById("field").value;
-    var message;
-    if (x == null || x == "") {
-        message = "Error! Field is empty!!!";
+function validate() {
+    var field = document.getElementById("field").value;
+    var error;
+    if (field == null || field == "") {
+        error = "Error! Field is empty!!!";
     }
-    else {
-        message = "";
-    }
-    document.getElementById("demo").innerHTML = message;
+    document.getElementById("demo").innerHTML = error;
 }
